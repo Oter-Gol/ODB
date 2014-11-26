b59c869a476889a648b82db363376b28b4b82f8d
oleh <olehgol260@gmail.com>
2014-11-27 00:36:40 +0200

    comments

--- src/tableClasses/Message.java
+++ src/tableClasses/Message.java
@@ -14,6 +14,15 @@ public class Message extends AbstractEntity<Integer> {
     private Profile sender;
     private Profile recipient;
 
+    /**
+     * constructor for Message
+     *
+     * @param themeOfMessage the theme of the message
+     * @param dateOfSending date of sending
+     * @param textOfMessage the text of the message
+     * @param sender profile of a sender
+     * @param recipient profile of a recipient
+     */
     public Message(String themeOfMessage, Date dateOfSending, String textOfMessage, Profile sender, Profile recipient) {
         this.themeOfMessage = themeOfMessage;
         this.dateOfSending = dateOfSending;
@@ -22,42 +31,81 @@ public class Message extends AbstractEntity<Integer> {
         this.recipient = recipient;
     }
 
+    /**
+     * getter for theme of the message
+     * @return themeOfMessage
+     */
     public String getThemeOfMessage() {
         return themeOfMessage;
     }
 
+    /**
+     * setter for theme of the message
+     * @param themeOfMessage which to set for the field
+     */
     public void setThemeOfMessage(String themeOfMessage) {
         this.themeOfMessage = themeOfMessage;
     }
 
+    /**
+     * getter for date of Sending of the message
+     * @return dateOfSending
+     */
     public Date getDateOfSending() {
         return dateOfSending;
     }
 
+    /**
+     * setter for date of sending
+     * @param dateOfSending which to set for the field
+     */
     public void setDateOfSending(Date dateOfSending) {
         this.dateOfSending = dateOfSending;
     }
 
+    /**
+     * getter for text of message
+     * @return textOfMessage
+     */
     public String getTextOfMesssage() {
         return textOfMessage;
     }
 
+    /**
+     * setter for text of the message
+     * @param textOfMesssage which to set for the field
+     */
     public void setTextOfMesssage(String textOfMesssage) {
         this.textOfMessage = textOfMesssage;
     }
 
+    /**
+     * getter for Profile sender
+     * @return sender
+     */
     public Profile getSender() {
         return sender;
     }
 
+    /**
+     * setter for Profile field sender
+     * @param sender who send a message
+     */
     public void setSender(Profile sender) {
         this.sender = sender;
     }
 
+    /**
+     *
+     * @return
+     */
     public Profile getRecipient() {
         return recipient;
     }
-
+    /**
+     * setter for Profile field recipient
+     * @param recipient who receive a message
+     */
     public void setRecipient(Profile recipient) {
         this.recipient = recipient;
     }
--- src/tableClasses/Profile.java
+++ src/tableClasses/Profile.java
@@ -10,32 +10,63 @@ public class Profile extends AbstractEntity<Integer>{
     private String password;
     private Category category;
 
+    /**
+     * constructor
+     *
+     * @param login of the user
+     * @param password of the user
+     * @param category investor or contact-manager
+     */
     public Profile(String login, String password, Category category) {
         this.login = login;
         this.password = password;
         this.category = category;
     }
 
+    /**
+     * getter for login
+     * @return login
+     */
     public String getLogin() {
         return login;
     }
 
+    /**
+     * setter for login
+     * @param login which to set
+     */
     public void setLogin(String login) {
         this.login = login;
     }
 
+    /**
+     * getter for password
+     * @return password
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * setter for password
+     * @param password which to set
+     */
     public void setPassword(String password) {
         this.password = password;
     }
 
+    /**
+     * getter for category
+     * @return category
+     */
     public Category getCategory() {
         return category;
     }
 
+    /**
+     * setter for category
+     * @param category which to set
+     */
     public void setCategory(Category category) {
         this.category = category;
     }
